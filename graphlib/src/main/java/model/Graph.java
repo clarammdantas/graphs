39424ee0509f2dc6369f43a5c1af4c73b80e245a
clarammdantas <clara.moraesd@gmail.com>
2018-05-18 01:21:35 -0300

    Add documentation

--- graphlib/src/main/java/model/Graph.java
+++ graphlib/src/main/java/model/Graph.java
@@ -16,6 +16,12 @@ public class Graph <T extends Edge> {
         this.edges = new ArrayList<T>();
     }
 
+    /**
+     * Get the adjacent vertices of a given vertex.
+     *
+     * @param vertex
+     * @return List of vertices that are adjacent to the given vertex
+     */
     public List<Vertex> getNeighbours(Vertex vertex) {
         List<Vertex> neighbours = new ArrayList();
         for (T edge : edges) {
@@ -29,10 +35,18 @@ public class Graph <T extends Edge> {
         return neighbours;
     }
 
+    /**
+     *
+     * @return Total number of vertices in the graph.
+     */
     public int getNumberOfVertices() {
         return numberOfVertices;
     }
 
+    /**
+     *
+     * @return A set with all vertices in the graph.
+     */
     public Set<Vertex> getVertices() {
         Set<Vertex> vertices = new HashSet();
         for (T edge : edges) {
@@ -59,10 +73,21 @@ public class Graph <T extends Edge> {
         this.edges = edges;
     }
 
+    /**
+     * Check whether a vertex value is valid or not. To be valid, the value must
+     * be between 1 and the total number of vertices, inclusive.
+     * @param vertex
+     * @return true if a vertex value is valid, false otherwise.
+     */
     private boolean validVertexNumber(Vertex vertex) {
         return vertex.getNumber() > 0 && vertex.getNumber() <= numberOfVertices;
     }
 
+    /**
+     * Add an edge into the graph.
+     * @param edge
+     * @throws GraphLibraryException
+     */
     public void addEdge(T edge) throws GraphLibraryException {
         if (validVertexNumber(edge.getEndpointA()) && validVertexNumber(edge.getEndpointB())) {
             if (edges.contains(edge)) {
