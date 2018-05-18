8e5520e303bf1789ddf8dc1398a078975324e75f
thiaguin <jose.thiago.silva@ccc.ufcg.edu.br>
2018-05-18 01:21:49 -0300

    Refatorando teste connected

--- graphlib/src/test/java/GraphLibraryTest.java
+++ graphlib/src/test/java/GraphLibraryTest.java
@@ -48,36 +48,6 @@ public class GraphLibraryTest {
 
 		return graph;
 	}
-    private Graph<Edge> buildGraph2() throws GraphLibraryException {
-        
-        Graph<Edge> graph = new Graph<Edge>(7);
-       
-        Vertex vertex1 = new Vertex(1);
-        Vertex vertex2 = new Vertex(2);
-        Vertex vertex3 = new Vertex(3);
-        Vertex vertex4 = new Vertex(4);
-        Vertex vertex5 = new Vertex(5);
-        Vertex vertex6 = new Vertex(6);
-        Vertex vertex7 = new Vertex(7);
- 
-       
-       
-        Edge edge1 = new Edge(vertex1, vertex2);
-        Edge edge2 = new Edge(vertex5, vertex1);
-        Edge edge3 = new Edge(vertex2, vertex5);
-        Edge edge4 = new Edge(vertex5, vertex3);
-        Edge edge5 = new Edge(vertex5, vertex4);
-        Edge edge6 = new Edge(vertex6, vertex7);
-       
-        graph.addEdge(edge1);
-        graph.addEdge(edge2);
-        graph.addEdge(edge3);
-        graph.addEdge(edge4);
-        graph.addEdge(edge5);
-        graph.addEdge(edge6);
- 
-        return graph;
-    }
     
     @Test
     public void readGraphShouldWorkProperly() throws FileNotFoundException, GraphLibraryException {
@@ -136,9 +106,18 @@ public class GraphLibraryTest {
 	 @Test
 	 public void testConnected() throws GraphLibraryException {
 		 Graph<Edge> graph = buildGraph();
-	     Graph<Edge> graph2 = buildGraph2();
-	     Assert.assertTrue(graphLibrary.connected(graph));      
-	     Assert.assertFalse(graphLibrary.connected(graph2));
+	     Assert.assertTrue(graphLibrary.connected(graph)); 
+	     
+	     graph.setNumberOfVertices(7);
+	     	     
+	     Vertex vertex6 = new Vertex(6);
+	     Vertex vertex7 = new Vertex(7);
+	     
+	     Edge edge5 = new Edge(vertex6, vertex7);
+			
+	     graph.addEdge(edge5);
+	     	   
+	     Assert.assertFalse(graphLibrary.connected(graph));
 	}
 
 }
